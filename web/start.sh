0f3dfa3511b39fbc6aa9093e3725163826a5d9ec
root <root@vps-919804ae.vps.ovh.net>
2023-10-12 21:04:10 +0000

    Dani

--- web/start.sh
+++ web/start.sh
@@ -1,3 +1,7 @@
-#!/bin/sh
-cd /var/www/flutter/sic4change/web
-flutter run -d web-server --web-hostname 0.0.0.0 --web-port 8989
+#!/bin/bash
+export PATH=/home/danidiaz/develop/flutter/bin/:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+while true
+do
+    cd /var/www/flutter/sic4change/web
+    flutter run -d web-server --web-hostname 0.0.0.0 --web-port 8989
+done
