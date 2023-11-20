b29dc4deede65ee7c0bac4839a5398b206805fb4
Daniel Díaz González <danidiaz@shidix.com>
2023-11-19 21:43:31 -0600

    Dani

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'dart:developer' as developer;
-import 'package:firebase_auth/firebase_auth.dart';
+// import 'dart:developer' as developer;
+// import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:sic4change/pages/index.dart';
 
