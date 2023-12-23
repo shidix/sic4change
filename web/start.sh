bb7f6fc41e8c7aac18175d5b195b86b495355ca8
Dani Diaz <danidiaz@vps-919804ae.vps.ovh.net>
2023-12-23 16:22:39 +0000

    Dani. WebStart

--- web/start.sh
+++ web/start.sh
@@ -2,7 +2,11 @@
 export PATH=/home/danidiaz/develop/flutter/bin/:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 while true
 do
+    #flutter build --release
+    #flutter build web
+    cd /var/www/flutter
+    flutter gen-l10n
     cd /var/www/flutter/sic4change/web
-    date
-    flutter run -d web-server --debug --web-hostname 0.0.0.0 --web-port 8989
+    flutter run -d web-server --web-hostname 0.0.0.0 --web-port 8989
+    #flutter run --release -d web-server --web-hostname 0.0.0.0 --web-port 8989
 done
