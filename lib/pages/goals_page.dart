55ff1ebf37747244db0b19d5e5b992d195ae54b5
zeben <zeben@shidix.com>
2024-01-09 23:22:17 +0000

    test

--- lib/pages/goals_page.dart
+++ lib/pages/goals_page.dart
@@ -71,8 +71,8 @@ class _GoalsPageState extends State<GoalsPage> {
           children: [
             /*addBtn(
                 context, editGoalDialog, {'_goal': null, '_project': project}),*/
-            addBtn(context, editGoalDialog,
-                {'goal': null, 'project': project.uuid}),
+            addBtn(
+                context, editGoalDialog, {'goal': "", 'project': project.uuid}),
             space(width: 10),
             returnBtn(context),
           ],
@@ -92,7 +92,7 @@ class _GoalsPageState extends State<GoalsPage> {
   Future<void> editGoalDialog(context, HashMap args) {
     //Goal goal = (args["goal"] != null) ? args["goal"] : Goal(args["project"]);
     Goal goal = Goal(args["project"]);
-    if (args["goal"] == null) {
+    if (args["goal"] != "") {
       goal = args["goal"];
     }
 
@@ -104,47 +104,44 @@ class _GoalsPageState extends State<GoalsPage> {
           titlePadding: const EdgeInsets.all(0),
           title: s4cTitleBar(
               (goal.name != "") ? 'Editando Objetivo' : 'Añadiendo Objetivo'),
-          content: StatefulBuilder(
-              builder: (BuildContext context, StateSetter setState) {
-            return SingleChildScrollView(
-                child: Row(children: <Widget>[
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                CustomTextField(
-                  labelText: "Nombre",
-                  initial: goal.name,
-                  size: 220,
-                  fieldValue: (String val) {
-                    setState(() => goal.name = val);
+          content: SingleChildScrollView(
+              child: Row(children: <Widget>[
+            Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              CustomTextField(
+                labelText: "Nombre",
+                initial: goal.name,
+                size: 220,
+                fieldValue: (String val) {
+                  setState(() => goal.name = val);
+                },
+              )
+            ]),
+            space(width: 20),
+            Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              CustomTextField(
+                labelText: "Descripción",
+                initial: goal.description,
+                size: 220,
+                fieldValue: (String val) {
+                  setState(() => goal.description = val);
+                },
+              )
+            ]),
+            Column(children: [
+              customText("Principal", 12),
+              FormField<bool>(builder: (FormFieldState<bool> state) {
+                return Checkbox(
+                  value: goal.main,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      goal.main = value!;
+                      state.didChange(goal.main);
+                    });
                   },
-                )
-              ]),
-              space(width: 20),
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                CustomTextField(
-                  labelText: "Descripción",
-                  initial: goal.description,
-                  size: 220,
-                  fieldValue: (String val) {
-                    setState(() => goal.description = val);
-                  },
-                )
-              ]),
-              Column(children: [
-                customText("Principal", 12),
-                FormField<bool>(builder: (FormFieldState<bool> state) {
-                  return Checkbox(
-                    value: goal.main,
-                    onChanged: (bool? value) {
-                      setState(() {
-                        goal.main = value!;
-                        state.didChange(goal.main);
-                      });
-                    },
-                  );
-                })
-              ]),
-            ]));
-          }),
+                );
+              })
+            ]),
+          ])),
           actions: <Widget>[dialogsBtns(context, saveGoal, goal)],
         );
       },
