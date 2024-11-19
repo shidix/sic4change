d7f8bba33598b3cefc8016375da48d42de32f529
Daniel Díaz González <danidiaz@shidix.com>
2024-11-19 07:17:04 -0600

    Dani

--- lib/pages/hierarchy_page.dart
+++ lib/pages/hierarchy_page.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:sic4change/generated/l10n.dart';
 import 'package:sic4change/services/models_commons.dart';
 import 'package:sic4change/services/models_profile.dart';
 import 'package:sic4change/services/models_rrhh.dart';
