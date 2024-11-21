9b79f927cea429e4cc7925f9e35a6c461f08705c
Daniel Díaz González <danidiaz@shidix.com>
2024-11-20 22:38:01 -0600

    Dani. Gestión de contactos

--- lib/pages/contacts_page.dart
+++ lib/pages/contacts_page.dart
@@ -11,6 +11,7 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 const pageContactTitle = "CRM Contactos de la organización";
 List orgs = [];
 List contacts = [];
+List allContacts = [];
 String currentOrg = "Ninguna seleccionada";
 bool orgsLoading = false;
 bool contactsLoading = false;
@@ -40,34 +41,33 @@ class _ContactsPageState extends State<ContactsPage> {
     });
   }
 
-  void loadContacts(value) async {
-    setState(() {
-      contactsLoading = true;
-    });
+  // void loadContacts(value) async {
+  //   setState(() {
+  //     contactsLoading = true;
+  //   });
 
-    if (value != "-1") {
-      await getContactsByOrg(value).then((val) {
-        contacts = val;
-        setState(() {
-          contactsLoading = false;
-        });
-      });
-    } else {
-      await getContacts().then((val) {
-        contacts = val;
-        setState(() {
-          contactsLoading = false;
-        });
-      });
-    }
-    if (mounted) {
-      setState(() {});
-    }
-    for (Contact c in contacts) {
-      await c.loadObjs();
-    }
-    setState(() {});
-  }
+  //   if (value != "-1") {
+  //     await getContactsByOrg(value).then((val) {
+  //       contacts = val;
+  //       setState(() {
+  //         contactsLoading = false;
+  //       });
+  //     });
+  //   } else {
+  //     await getContacts().then((val) {
+  //       contacts = val;
+  //       setState(() {
+  //         contactsLoading = false;
+  //       });
+  //     });
+  //   }
+  //   for (Contact c in contacts) {
+  //     await c.loadObjs();
+  //   }
+  //   if (mounted) {
+  //     setState(() {});
+  //   }
+  // }
 
   void findOrganizations(value) async {
     setState(() {
@@ -86,20 +86,60 @@ class _ContactsPageState extends State<ContactsPage> {
     setState(() {
       contactsLoading = true;
     });
+    print("DBG:> $value");
 
-    searchContacts(value).then((val) {
-      contacts = val;
+    if (value == "") {
+      contacts = allContacts;
+    } else {
+      contacts = allContacts
+          .where((element) =>
+              (element.name.toLowerCase().contains(value.toLowerCase()) ||
+                  element.email.toLowerCase().contains(value.toLowerCase()) ||
+                  element.organizationObj.name
+                      .toLowerCase()
+                      .contains(value.toLowerCase()) ||
+                  element.companyObj.name
+                      .toLowerCase()
+                      .contains(value.toLowerCase()) ||
+                  element.positionObj.name
+                      .toLowerCase()
+                      .contains(value.toLowerCase()) ||
+                  element.phone.toLowerCase().contains(value.toLowerCase())))
+          .toList();
+    }
+    if (mounted) {
       setState(() {
         contactsLoading = false;
       });
-    });
+    }
+
+    // searchContacts(value).then((val) {
+    //   contacts = val;
+    //   setState(() {
+    //     contactsLoading = false;
+    //   });
+    // });
   }
 
   @override
   void initState() {
     _mainMenu = mainMenu(context, "/contacts");
+    if (contacts.isEmpty) {
+      Contact.getAll().then((val) {
+        allContacts = val;
+        for (Contact c in allContacts) {
+          c.loadObjs().then((value) {
+            contacts.add(c);
+            if (mounted) {
+              setState(() {});
+            }
+          });
+        }
+      });
+    }
     loadOrgs();
-    loadContacts("");
+
+    // loadContacts("-1");
     super.initState();
   }
 
@@ -169,6 +209,7 @@ class _ContactsPageState extends State<ContactsPage> {
                     context,
                     callEditOrgDialog,
                     {'org': Organization("")},
+                    text: "",
                   ),
                 ])),
         Container(
@@ -218,7 +259,14 @@ class _ContactsPageState extends State<ContactsPage> {
                         onSelectChanged: (bool? selected) {
                           if (selected == true) {
                             currentOrg = org.name;
-                            loadContacts(org.uuid);
+                            contacts = allContacts
+                                .where((element) =>
+                                    element.organizationObj.uuid == org.uuid)
+                                .toList();
+                            if (mounted) {
+                              setState(() {});
+                            }
+                            //loadContacts(org.uuid);
                           }
                         },
                         cells: [
@@ -355,10 +403,16 @@ class _ContactsPageState extends State<ContactsPage> {
 -------------------------------------------------------------*/
   void filterContacts(context, args) {
     if (args["filter"] == "all") {
-      loadContacts("-1");
+      findContacts("");
+      //loadContacts("-1");
     }
     if (args["filter"] == "generic") {
-      loadContacts("");
+      contacts = allContacts
+          .where((element) => element.organizationObj.name == "")
+          .toList();
+      if (mounted) {
+        setState(() {});
+      }
     }
   }
 
@@ -375,11 +429,14 @@ class _ContactsPageState extends State<ContactsPage> {
                       width: 500,
                       child: SearchBar(
                         controller: searchController,
-                        padding: const MaterialStatePropertyAll<EdgeInsets>(
+                        padding: const WidgetStatePropertyAll<EdgeInsets>(
                             EdgeInsets.symmetric(horizontal: 12.0)),
-                        onSubmitted: (value) {
+                        onChanged: (value) {
                           findContacts(value);
                         },
+                        // onSubmitted: (value) {
+                        //   findContacts(value);
+                        // },
                         leading: const Icon(Icons.search),
                       )),
                   addBtnRow(context, filterContacts, {"filter": "generic"},
@@ -491,7 +548,13 @@ class _ContactsPageState extends State<ContactsPage> {
   void saveContact(List args) async {
     Contact contact = args[0];
     contact.save();
-    loadContacts("");
+    contact.loadObjs().then((value) {
+      allContacts.add(contact);
+      findContacts("");
+      setState(() {});
+    });
+
+    // loadContacts("");
 
     Navigator.pop(context);
   }
@@ -592,7 +655,15 @@ class _ContactsPageState extends State<ContactsPage> {
   }
 
   void removeContactDialog(context, args) {
-    customRemoveDialog(context, args["contact"], loadContacts, "-1");
+    //customRemoveDialog(context, args["contact"], loadContacts, "-1");
+    Contact contact = args["contact"];
+    customRemoveDialog(context, args["contact"], () {
+      contacts.remove(contact);
+      allContacts.remove(contact);
+      if (mounted) {
+        setState(() {});
+      }
+    }, null);
   }
 
   /*Future<void> _removeContactDialog(context, _contact) async {
