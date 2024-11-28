597ec97b6b6b542842bd345a560e317b75f143fb
Dani Diaz <d.diaz@irya.unam.mx>
2024-11-28 09:28:01 -0600

    Dani

--- lib/pages/admin_town_page.dart
+++ lib/pages/admin_town_page.dart
@@ -140,7 +140,7 @@ class _TownPageState extends State<TownPage>
           sortColumnIndex: 0,
           showCheckboxColumn: false,
           headingRowColor:
-              MaterialStateColor.resolveWith((states) => headerListBgColor),
+              WidgetStateColor.resolveWith((states) => headerListBgColor),
           headingRowHeight: 40,
           columns: [
             DataColumn(
